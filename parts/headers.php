8080023d688bb6bf73b8b3e559b31030abbe3b2f
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-05 10:20:37 -0700

    Remove temporary main header name container
    We can add this back in once it is built out in the style guide

--- parts/headers.php
+++ parts/headers.php
@@ -3,7 +3,6 @@
 ?>
 <header class="main-header">
 	<div class="hgroup">
-		<!--<div class="main-header-name">WSU College of Medicine Style Guide</div>-->
 		<h1 class="main-header-description"><?php the_title(); ?></h1>
 	</div>
 </header>
