18214b000159f6bbe96a4ff38563576014e707ba
Jeremy Felt <jeremy.felt@gmail.com>
2016-10-11 15:54:27 -0700

    Begin refactoring JS behavior on standard screen sizes

--- src/js/ui.spine.framework.js
+++ src/js/ui.spine.framework.js
@@ -389,7 +389,6 @@
 		 * @since 0.0.3
 		 */
 		setup_standard_navigation: function() {
-			var self = this;
 
 			// Apply the `parent` class to each parent list item of an unordered list in the navigation.
 			$( "#spine nav ul, #spine ul" ).parents( "li" ).addClass( "parent" );
@@ -400,9 +399,6 @@
 			 * @type {any}
 			 */
 			var couplets = $( "#spine nav li.parent > a" );
-
-			// Assign active elements a class of dogeared unless those elements contain other active elements.
-			$( "#spine .active:not(:has(.active))" ).addClass( "dogeared" );
 
 			/**
 			 * Walk through each of the anchor elements in the navigation to establish when "Overview"
@@ -420,11 +416,6 @@
 
 				var classes = "overview";
 
-				// If a generated overview's parent is marked as dogeared, do the same with the overview.
-				if ( tar.closest( ".parent" ).is( ".dogeared" ) ) {
-					classes += " dogeared";
-				}
-
 				title = ( tar.is( "[title]" )  ) ? tar.attr( "title" ) : "Overview";
 				title = ( tar.is( "[data-overview]" ) ) ? tar.data( "overview" ) : title;
 				title = title.length > 0 ? title : "Overview"; // This is just triple checking that a value made it here.
@@ -432,51 +423,15 @@
 				tar.parent( "li" ).children( "ul" ).prepend( "<li class='" + classes + "'></li>" );
 				tar.clone( true, true ).appendTo( tar.parent( "li" ).find( "ul .overview:first" ) );
 				tar.parent( "li" ).find( "ul .overview:first a" ).html( title );
-
-				// When the overview page is active, that area of the navigation should be opened.
-				if ( tar.parent( "li" ).hasClass( "active" ) ) {
-					tar.parents( "li" ).removeClass( "active" ).addClass( "opened dogeared" );
-				}
 			} );
 
-			/**
-			 * Account for historical markup in the WSU ecosystem and add the `active` and `dogeared` classes
-			 * to any list items that already have classes similar to `current` or `active`. Also apply the
-			 * `opened` and `dogeared` classes to any parent list items of these active elements.
-			 *
-			 * `active` and `dogeared` are both used for the styling of active menu items in the navigation.
-			 */
-			$( "#spine nav li[class*=current], #spine nav li[class*=active]" ).addClass( "active dogeared" ).parents( "li" ).addClass( "opened dogeared" );
+			$( "#spine nav .active" ).parents( "li" ).addClass( "active" );
 
-			/**
-			 * Also look for any anchor elements using a similar method and apply `active` and `dogeared` classes to
-			 * all parent list items.
-			 */
-			$( "#spine nav li a[class*=current], #spine nav li a[class*=active]" ).parents( "li" ).addClass( "active dogeared" );
-
-			/**
-			 * Setup navigation events depending on what the screen size is when the document first
-			 * loads. If mobile, we use touch events for navigation. If not mobile, we rely on
-			 * standard click events.
-			 *
-			 * Some additional handling is necessary on mobile to properly handle the sequence of
-			 * touchstart, touchmove, and touchend without confusion.
-			 */
-			if ( self.is_mobile_view() ) {
-				couplets.on( "mousedown touchstart", function( e ) {
-					$( e.target ).on( "mouseup touchend", $.ui.spine.prototype._toggle_spine_nav_list );
-					$( e.target ).on( "mousemove touchmove", function( e ) {
-						$( e.target ).off( "mouseup touchend", $.ui.spine.prototype._toggle_spine_nav_list );
-					} );
-				} );
-			} else {
-
-				// Disclosure
-				couplets.on( "click", function( e ) {
-					e.preventDefault();
-					$( e.target ).closest( "li" ).toggleClass( "opened" );
-				} );
-			}
+			// Disclosure
+			couplets.on( "click", function( e ) {
+				e.preventDefault();
+				$( e.target ).closest( "li" ).toggleClass( "opened" );
+			} );
 
 			// Mark external URLs in the nav menu.
 			$( ".spine-navigation a[href^='http']:not([href*='://" + window.location.hostname + "'])" ).addClass( "external" );
