ac7b6e32726a4fb55c52f8731fa48a9342d5e1fa
Phil Cable <pcable@wsu.edu>
2016-10-26 13:01:48 -0700

    Remove the scroll event after animating the graph
    This commit also refactors and renames functions so that things
    are more consistent.

--- js/graph.js
+++ js/graph.js
@@ -3,7 +3,7 @@
 	/**
 	 * Check if the graph is in the viewport.
 	 */
-	var in_viewport = function( graph ) {
+	function inViewport( graph ) {
 		if ( graph instanceof jQuery ) {
 			graph = graph[ 0 ];
 		}
@@ -14,45 +14,7 @@
 			rect.right > 0 &&
 			rect.left < ( window.innerWidth || document.documentElement.clientWidth ) &&
 			rect.top < ( window.innerHeight || document.documentElement.clientHeight );
-	};
-
-	/**
-	 * Animate the graph when it is scrolled into the viewport.
-	 */
-	var animate_graph = function() {
-		var $graph = $( ".bargraph" ),
-			total = $graph.data( "total" ),
-			prefix = $graph.data( "prefix" );
-
-		$( document ).on( "scroll", function() {
-			if ( !in_viewport( $graph ) || $graph.hasClass( "animated" ) ) {
-				return;
-			}
-
-			$graph.addClass( "animated" ).find( "li" ).each( function( i ) {
-				var $bar = $( this ),
-					$value = $bar.find( ".value" ),
-					amount = $value.data( "amount" ),
-					percentage = amount / total * 100;
-
-				// Fire each animation at short intervals.
-				setTimeout( function() {
-
-					// Animate the bar's width to it's final percentage.
-					$bar.css( "width", percentage + "%" );
-
-					// Animate the element's value from x to y:
-					$( { currentAmount: 0 } ).animate( { currentAmount: amount }, {
-						duration: 1500,
-						easing: "swing",
-						step: function() {
-							$value.text( prefix + commaSeparateNumber( Math.round( this.currentAmount ) ) );
-						}
-					} );
-				}, i * 150 );
-			} );
-		} );
-	};
+	}
 
 	/**
 	 * Returns a comma separated number.
@@ -70,10 +32,51 @@
 	}
 
 	/**
+	 * Animate the graph.
+	 */
+	function animateGraph( event ) {
+		var graph = event.data.graph;
+
+		if ( !inViewport( graph ) ) {
+			return;
+		}
+
+		var total = graph.data( "total" ),
+			prefix = graph.data( "prefix" );
+
+		graph.addClass( "animated" ).find( "li" ).each( function( i ) {
+			var $bar = $( this ),
+				$value = $bar.find( ".value" ),
+				amount = $value.data( "amount" ),
+				percentage = amount / total * 100;
+
+			// Fire each animation at short intervals.
+			setTimeout( function() {
+
+				// Animate the bar's width to it's final percentage.
+				$bar.css( "width", percentage + "%" );
+
+				// Animate the element's value from x to y:
+				$( { currentAmount: 0 } ).animate( { currentAmount: amount }, {
+					duration: 1500,
+					easing: "swing",
+					step: function() {
+						$value.text( prefix + commaSeparateNumber( Math.round( this.currentAmount ) ) );
+					}
+				} );
+			}, i * 150 );
+		} );
+
+		$( window ).off( "scroll", animateGraph );
+	}
+
+	/**
 	 * Fire any actions that we need to happen once the document is ready.
 	 */
 	$( document ).ready( function() {
-		animate_graph();
+		var $graph = $( ".bargraph" );
+
+		$( window ).on( "scroll", { graph: $graph }, animateGraph );
 	} );
 }( jQuery, window, document ) );
 
