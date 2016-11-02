82b25b63eb975bd9caee3aafa4a1ee4c5dbdabc4
Jeremy Felt <jeremy.felt@gmail.com>
2016-11-02 11:09:03 -0700

    Actually filter the SVG divider

--- parts/footers.php
+++ parts/footers.php
@@ -1,7 +1,11 @@
 <?php
 
+$breadcrumb_display = apply_filters( 'medicine_filter_breadcrumb', bcn_display( true ) );
+
+// @codingStandardsIgnoreStart
 if ( ! is_front_page() && ! is_home() && spine_display_breadcrumbs( 'bottom' ) ) {
 	?><section class="row single breadcrumbs breadcrumbs-bottom gutter pad-top" typeof="BreadcrumbList" vocab="http://schema.org/">
-	<div class="column one"><?php bcn_display(); ?></div>
+	<div class="column one"><?php echo $breadcrumb_display; ?></div>
 	</section><?php
 }
+// @codingStandardsIgnoreEnd
