e0d0353b95475317fe14cd53b2921b80b44b810c
Jeremy Felt <jeremy.felt@gmail.com>
2016-11-01 17:20:11 -0700

    Bump version 0.1.4

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@ include_once( __DIR__ . '/includes/class-medicine-bar-graph-shortcode.php' );
 
 add_filter( 'spine_child_theme_version', 'medicine_theme_version' );
 function medicine_theme_version() {
-	return '0.1.3';
+	return '0.1.4';
 }
 
 add_action( 'init', 'medicine_remove_spine_wp_enqueue_scripts' );
