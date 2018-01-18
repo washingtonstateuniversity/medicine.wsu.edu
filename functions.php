25864392c358727e06b2b42332ad019fa014e30d
Phil Cable <pcable@wsu.edu>
2018-01-18 12:01:16 -0800

    Bump version 0.2.2

--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/includes/constant-contact.php';
 
 add_filter( 'spine_child_theme_version', 'medicine_theme_version' );
 function medicine_theme_version() {
-	return '0.2.1';
+	return '0.2.2';
 }
 
 add_action( 'init', 'medicine_remove_spine_wp_enqueue_scripts' );
