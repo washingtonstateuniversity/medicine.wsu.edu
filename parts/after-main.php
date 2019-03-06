22be3bf263b472dccb2d9ba085879c99985dbeeb
danialbleile <danial.bleile@wsu.edu>
2019-03-05 16:20:56 -0800

    Add social icons to footer

--- parts/after-main.php
+++ parts/after-main.php
@@ -18,26 +18,42 @@ $medicine_footer_menu_args = array(
 	<div class="wsu-signature">
 		<a href="https://wsu.edu"><img src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/wsu-logo-wht.svg" alt="Washington State University logo"></a>
 	</div>
-	<address class="footer-contact-info">
-		<span class="dept-name address-item"><?php echo esc_html( spine_get_option( 'contact_department' ) ); ?></span>
-		<span class="street-address address-item"><?php
-		if ( spine_get_option( 'contact_streetAddress' ) !== '' ) {
-			echo esc_html( spine_get_option( 'contact_streetAddress' ) . ' ' );
-		}
-		if ( spine_get_option( 'contact_addressLocality' ) !== '' ) {
-			echo esc_html( spine_get_option( 'contact_addressLocality' ) . ' ' );
-		}
-		if ( spine_get_option( 'contact_postalCode' ) !== '' ) {
-			echo esc_html( spine_get_option( 'contact_postalCode' ) );
-		}
-		?></span>
-		<?php if ( spine_get_option( 'contact_email' ) !== '' ) { ?>
-		<a class="email-address address-item" href="mailto:<?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?>"><?php echo esc_html( spine_get_option( 'contact_email' ) ); ?></a>
-		<?php } ?>
-		<?php if ( spine_get_option( 'contact_telephone' ) !== '' ) { ?>
-		<a class="telephone-number address-item" href="tel:<?php echo esc_attr( spine_get_option( 'contact_telephone' ) ); ?>"><?php echo esc_html( spine_get_option( 'contact_telephone' ) ); ?></a>
-		<?php } ?>
-	</address>
+	<div class="footer-contact-wrapper">
+		<ul class="social-media">
+					<li>
+				<a class="ripple facebook" href="https://facebook.com/wsumedicine">Facebook</a>
+			</li>
+					<li>
+				<a class="ripple twitter" href="https://twitter.com/wsumedicine">Twitter</a>
+			</li>
+					<li>
+				<a class="ripple instagram" href="https://instagram.com/wsumedicinen">Instagram</a>
+			</li>
+					<li>
+				<a class="ripple back-to-top" href="#">Back to top</a>
+			</li>
+		</ul>
+		<address class="footer-contact-info">
+			<span class="dept-name address-item"><?php echo esc_html( spine_get_option( 'contact_department' ) ); ?></span>
+			<span class="street-address address-item"><?php
+			if ( spine_get_option( 'contact_streetAddress' ) !== '' ) {
+				echo esc_html( spine_get_option( 'contact_streetAddress' ) . ' ' );
+			}
+			if ( spine_get_option( 'contact_addressLocality' ) !== '' ) {
+				echo esc_html( spine_get_option( 'contact_addressLocality' ) . ' ' );
+			}
+			if ( spine_get_option( 'contact_postalCode' ) !== '' ) {
+				echo esc_html( spine_get_option( 'contact_postalCode' ) );
+			}
+			?></span>
+			<?php if ( spine_get_option( 'contact_email' ) !== '' ) { ?>
+			<a class="email-address address-item" href="mailto:<?php echo esc_attr( spine_get_option( 'contact_email' ) ); ?>"><?php echo esc_html( spine_get_option( 'contact_email' ) ); ?></a>
+			<?php } ?>
+			<?php if ( spine_get_option( 'contact_telephone' ) !== '' ) { ?>
+			<a class="telephone-number address-item" href="tel:<?php echo esc_attr( spine_get_option( 'contact_telephone' ) ); ?>"><?php echo esc_html( spine_get_option( 'contact_telephone' ) ); ?></a>
+			<?php } ?>
+		</address>
+	</div>
 	<nav class="global-links">
 		<ul>
 			<li><a href="https://wsu.edu">Washington State University</a></li>
